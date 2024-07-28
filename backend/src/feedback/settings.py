3f7831136cae60938965a892c22c0c595ece138f
Vernadskii <d.vernadskii@gmail.com>
2024-07-28 14:34:59 +0300

    feat: add linters to pyproject.toml with configurations (#5)
    * feat: add linters to pyproject.toml with configurations
    * fix: fixed isort violations
    * feat: added ignoring pycharm idea folder
    * feat: corrected django auto-generated code accordance with linters

--- backend/src/feedback/settings.py
+++ backend/src/feedback/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
 }
 
 
