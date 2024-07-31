57bc448faedd5235c2940dec0718270c05349eda
Vernadskii <d.vernadskii@gmail.com>
2024-07-31 19:23:50 +0300

    feat: add linter job (#18)
    * feat: add linter job and improved linter check

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -5,6 +5,8 @@ if [ "$1" = "run" ]; then
   exec poetry run /app/src/manage.py runserver 0.0.0.0:8000
 elif [ "$1" = "migrate" ]; then
   exec poetry run /app/src/manage.py migrate
+elif [ "$1" = "linters" ]; then
+  exec poetry run flake8
 else
   exec "$@"
 fi
