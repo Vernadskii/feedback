4dec68d55cc28dea1989a71483ffb1f047cea88b
eeaeoa <142530018+eeaeoa@users.noreply.github.com>
2024-08-11 19:11:21 +0300

    feat: add eslint plugins and prettier for code quality and formatting (#21)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   server: {
     port: 5173,
-    host: '0.0.0.0',
+    host: '0.0.0.0'
   },
+  resolve: {
+    alias: {
+      assets: '/src/assets'
+    }
+  }
 })
