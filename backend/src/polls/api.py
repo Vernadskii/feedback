a155966cc7e54bf0810dfc249df71f022ada8784
Vernadskii <d.vernadskii@gmail.com>
2024-10-01 18:54:04 +0300

    feat: added put request for poll

--- backend/src/polls/api.py
+++ backend/src/polls/api.py
@@ -19,6 +19,19 @@ def create_poll(request, payload: BasePollSchema):
     return 201, ExistingPollSchema.model_validate(poll)
 
 
+@router.put("/{poll_id}", response={200: ExistingPollSchema, 400: str}, auth=AuthBearer())
+def update_poll(request, poll_id: int, payload: BasePollSchema):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    try:
+        # Update the instance
+        for attr, value in payload.dict().items():
+            setattr(poll, attr, value)
+        poll.save()
+    except Exception as e:
+        return 400, f"An unexpected error occurred: {str(e)}"
+    return 200, ExistingPollSchema.model_validate(poll)
+
+
 @router.get("/{poll_id}", response={200: ExistingPollSchema, 404: str}, auth=AuthBearer())
 def get_poll(request, poll_id: int):
     poll = get_object_or_404(Poll, id=poll_id)
@@ -26,6 +39,6 @@ def get_poll(request, poll_id: int):
 
 
 @router.get("/", response=list[ExistingPollSchema], auth=AuthBearer())
-@paginate(PageNumberPagination, page_size=1)
+@paginate(PageNumberPagination, page_size=15)
 def list_polls(request):
     return Poll.objects.all()
